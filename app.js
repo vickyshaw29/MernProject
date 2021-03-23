c130325ee331015ac047918f22a00d7d1330ef17
vickyshaw29 <prasadvicky231@gmail.com>
2021-03-22 22:36:35 -0700

    fix4

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const dirname = path.resolve()
 if(process.env.NODE_ENV==='production'){
     app.use(express.static(path.join(__dirname,'dist/build')))
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'dist','backend', 'build','index.html'))
+        res.sendFile(path.resolve(__dirname,'dist', 'build','index.html'))
     }) 
 }else{
     app.get('/',(req,res)=>{
